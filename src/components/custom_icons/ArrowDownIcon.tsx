4c738942ac32d9fffe2240974d35d40e3664e58d
Brad <klausmickelson52@gmail.com>
2024-04-24 01:18:53 +0100

    chore: setting up components

--- src/components/custom_icons/ArrowDownIcon.tsx
+++ src/components/custom_icons/ArrowDownIcon.tsx
@@ -12,7 +12,7 @@ const ArrowDownIcon = ({ className }: { className?: string }) => {
     >
       <path
         d="M8.0002 4H9.7802C11.9869 4 12.8935 5.56667 11.7869 7.48L10.8935 9.02L10.0002 10.56C8.89353 12.4733 7.08686 12.4733 5.9802 10.56L5.08686 9.02L4.19353 7.48C3.10686 5.56667 4.00686 4 6.2202 4H8.0002Z"
-        stroke="#0294E2"
+        stroke="currentColor"
         strokeMiterlimit="10"
         strokeLinecap="round"
         strokeLinejoin="round"
